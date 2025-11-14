03ed21f8ce400428996583104c5f4ac3195ed495
NguyenPhuocDaiToan <daitoan2000@gmail.com>
2025-11-14 15:39:43 +0700

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   urlBaseImage: 'https://backend-1-fljj.onrender.com/',
-  apiUrl: 'https://backend-1-fljj.onrender.com//v1/admin',
+  apiUrl: 'https://backend-1-fljj.onrender.com/v1/admin',
 };
