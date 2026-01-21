d5e239badcb2bdf8fdc4f79039b4ad57b8d4e525
NguyenPhuocDaiToan <daitoan2000@gmail.com>
2026-01-21 23:34:59 +0700

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // urlBaseImage: 'https://backend-1-fljj.onrender.com',
   // urlBaseImage: 'http://localhost:3000',
-  urlBaseImage: 'https://admin.luuchi.com.vn',
+  urlBaseImage: 'https://luuchi.com.vn',
   apiUrl: 'https://admin.luuchi.com.vn/v1/admin',
   socketUrl: 'https://admin.luuchi.com.vn',
 };
